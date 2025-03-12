9639fa724b344f551591f0bb0e88fd3746dc3b37
Chetan Rajauria <chetanrajauria@gmail.com>
2025-03-12 20:21:59 +0530

    Initial Commit

---
+++ SwiftUIAnimation/SwiftUIAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationApp.swift
+//  SwiftUIAnimation
+//
+//  Created by Ruchi Kumar on 12/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
